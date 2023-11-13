d41a8bcb1ddafc7f8e52c69dd909c60e787269a3
Rubensjr <145869584+jrRubens454@users.noreply.github.com>
2023-11-13 12:55:31 -0400

    Add files via upload

--- banco_de_dados/restaurante.php
+++ banco_de_dados/restaurante.php
@@ -1,42 +1,91 @@
 <<?php 
-CREATE TABLE cadastro_usuario (
+<?php
+$servername = "Login";
+$username = "scarlet";
+$password = "122523";
+$dbname = "MYsql://localhost3000";
+
+
+$conn = new mysqli($servername, $username, $password);
+
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+
+$sqlCreateDB = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sqlCreateDB) === TRUE) {
+    echo "Banco de dados criado com sucesso!";
+} else {
+    echo "Erro ao criar o banco de dados: " . $conn->error;
+}
+
+$conn->select_db($dbname);
+
+$sqlCreateTableUsuario = "CREATE TABLE IF NOT EXISTS usuario (
     id_usuario INT PRIMARY KEY,
     nome VARCHAR(100),
     email VARCHAR(100),
     telefone VARCHAR(20),
     senha VARCHAR(20)
-);
-CREATE TABLE login_usuario (
-    id_login INT PRIMARY KEY,
-    id_usuario INT,
-    email VARCHAR(100),
-    senha VARCHAR(20),
-    FOREIGN KEY (id_usuario) REFERENCES cadastro_usuario(id_usuario)
-);
-CREATE TABLE restaurante (
-    id_restaurante INT PRIMARY KEY,
-    cardapio TEXT,
-    endereco VARCHAR(200),
-    mesa INT,
-    data_hora DATETIME
-);
-CREATE TABLE restaurante (
-    id_restaurante INT PRIMARY KEY,
-    cardapio TEXT,
-    endereco VARCHAR(200),
-    mesa INT,
-    data_hora DATETIME
-);
-CREATE TABLE reserva (
+)";
+if ($conn->query($sqlCreateTableUsuario) === TRUE) {
+    echo "Tabela 'usuario' criada com sucesso!";
+} else {
+    echo "Erro ao criar a tabela 'usuario': " . $conn->error;
+}
+
+$sqlCreateTableReserva = "CREATE TABLE IF NOT EXISTS reserva (
     id_reserva INT PRIMARY KEY,
-    id_restaurante INT,
     id_usuario INT,
     mesa INT,
     data_hora DATETIME,
     numero_pessoas INT,
-    FOREIGN KEY (id_restaurante) REFERENCES restaurante(id_restaurante),
-    FOREIGN KEY (id_usuario) REFERENCES cadastro_usuario(id_usuario)
-);
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+)";
+if ($conn->query($sqlCreateTableReserva) === TRUE) {
+    echo "Tabela 'reserva' criada com sucesso!";
+} else {
+    echo "Erro ao criar a tabela 'reserva': " . $conn->error;
+}
+
+$conn->close();
+?>
+<?php
+include 'conexao.php';
+
+$sql = "SELECT * FROM reserva";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>
+            <tr>
+                <th>ID Reserva</th>
+                <th>ID Usuário</th>
+                <th>Mesa</th>
+                <th>Data e Hora</th>
+                <th>Número de Pessoas</th>
+            </tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>" . $row["id_reserva"] . "</td>
+                <td>" . $row["id_usuario"] . "</td>
+                <td>" . $row["mesa"] . "</td>
+                <td>" . $row["data_hora"] . "</td>
+                <td>" . $row["numero_pessoas"] . "</td>
+              </tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "Nenhuma reserva encontrada.";
+}
+
+// Fecha a conexão com o banco de dados
+$conn->close();
+?>
 
 
 ?>
